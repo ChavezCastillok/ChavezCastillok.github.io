7c6f7217058cb86b1676081d7e33d5fae55fddd3
Kevin Chavez Castillo <socialcck@outlook.com>
2020-03-03 11:06:42 -0400

    Final de la conversion del sitio de neocities a la pagina unica en GitHub

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*
+product by @chavezCastillok
+*/
+
+window.addEventListener('load', function(){
+
+    document.getElementById('currentYear').innerHTML = new Date().getFullYear()
+
+    console.log('Web site charged completed.');
+});
